ba48854346d8595106ddb64c92edc20f2ae5efe3
smaz-beep <smazeth@gmail.com>
2026-09-20 13:09:46 +0200

    v16
    lm eintragen & Verwaltung komplett überarbeitet. darkmode integriert; protokoll anischt ein bisschen angepasst

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tracking-app-v016-10';
+const CACHE_NAME = 'tracking-app-v016-44';
 
 // Ohne diese Dateien laeuft die App nicht - fehlt eine, soll die Installation scheitern
 const CORE_ASSETS = [
